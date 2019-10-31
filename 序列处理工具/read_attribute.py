654a1c137ee3d51f3c661704a3af72b14cdfcb94
zeng <guibuzi@outlook.com>
2019-10-31 11:05:04 +0800

    manage file

--- 序列处理工具/read_attribute.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_csv(r"D:\python_work\暑期实习\H3N2_to2019_global\processed data\Isolated-information\ioslate_information_处理.csv")
--- 序列处理工具/ss.py
+++
@@ -1,9 +0,0 @@
-def power_sum(*numbers):
-    sum = 0
-    for n in numbers:
-        sum = sum + n*n
-    return sum
-
-
-a = power_sum(1, 2, 3)
-print(a)

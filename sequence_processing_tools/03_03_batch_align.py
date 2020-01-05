ade7f9b88c4dec361be97538c209adce747a3111
zeng <guibuzi@outlook.com>
2020-01-05 16:38:05 +0800

    reassortment analysis by region

--- sequence_processing_tools/03_03_batch_align.py
+++ sequence_processing_tools/03_03_batch_align.py
@@ -8,28 +8,39 @@ def muscle(_in, _out):
     print("Finsh align %s" % os.path.split(_in)[1])
 
 
-path = "/home/zeng/Desktop/sample2"
-out_path = "/home/zeng/Desktop/sample2/align"
+def generate_path(path, out_path):
+    os.system("rm -rf %s/align" % path)
+    tasks = os.listdir(path)
+    in_seqs = []
+    out_seqs = []
 
-os.system("rm -rf /home/zeng/Desktop/sample2/align")
+    for task in tasks:
+        _in = os.path.join(path,  task)
+        _out = os.path.join(out_path, task)
+        in_seqs.append(_in)
+        out_seqs.append(_out)
 
-tasks = os.listdir(path)
-in_seqs = []
-out_seqs = []
+    os.makedirs("%s/align" % path, exist_ok=True)
+    return in_seqs, out_seqs
 
-for task in tasks:
-    _in = os.path.join(path,  task)
-    _out = os.path.join(out_path, task)
-    in_seqs.append(_in)
-    out_seqs.append(_out)
 
-os.makedirs("/home/zeng/Desktop/sample2/align", exist_ok=True)
+def batch_muscle(in_seqs, out_seqs, num_threads=4):
+    pool = Pool(processes = num_threads)
+    print("Begin tasks ...")
+    for i, o in zip(in_seqs, out_seqs):
+        pool.apply_async(muscle, (i, o))
+    pool.close()
+    pool.join()
+    print("Finsh muscle.")
 
-pool = Pool(processes = 6)
-print("Begin tasks ...")
-for i, o in zip(in_seqs, out_seqs):
-    pool.apply_async(muscle, (i, o))
 
-pool.close()
-pool.join()
-print("Finsh muscle.")
+def main(path, out_path):
+    in_seqs, out_seqs = generate_path(path, out_path)
+    batch_muscle(in_seqs, out_seqs)
+
+
+if __name__ == "__main__":
+    main("/home/zeng/Desktop/sample_aus", "/home/zeng/Desktop/sample_aus/align")
+    main("/home/zeng/Desktop/sample_europe", "/home/zeng/Desktop/sample_europe/align")
+    main("/home/zeng/Desktop/sample_seasia", "/home/zeng/Desktop/sample_seasia/align")
+    main("/home/zeng/Desktop/sample_usa", "/home/zeng/Desktop/sample_usa/align")
